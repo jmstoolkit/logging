15653d5969199407281a28e76ad6143eeab15020
Scott Douglass <scott@swdouglass.com>
2017-11-07 07:38:14 -0500

    mavenized

---
+++ orson/install.sh
@@ -0,0 +1,7 @@
+mvn install:install-file \
+  -Dfile=orson-0.6.0.jar \
+  -DgroupId=org.jfree \
+  -DartifactId=orson \
+  -Dversion=0.6.0 \
+  -Dpackaging=jar \
+  -DgeneratePom=true
--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-JAR="jmstoolkit-logging.jar"
-COMMAND="-jar $JAR"
+JAR="jmstoolkit-logging-jar-with-dependencies.jar"
+for J in `ls lib/*.jar 2>/dev/null`; do
+  CLASSPATH=${J}:${CLASSPATH}
+done
+CLASSPATH="`pwd`/${JAR}:${CLASSPATH}"
 # Set to the directory where your JMS provider jar files are
 #JMS_PROVIDER_DIR=`pwd`/activemq
 if [ "X${JMS_PROVIDER_DIR}" != "X" ]; then
-  for J in `ls lib/*.jar`; do
-    CLASSPATH=${J}:${CLASSPATH}
-  done
   for J in `ls ${JMS_PROVIDER_DIR}/*.jar`; do
     CLASSPATH=${J}:${CLASSPATH}
   done
   CLASSPATH="`pwd`/${JAR}:${CLASSPATH}"
-  echo "CLASSPATH: $CLASSPATH"
-  export CLASSPATH
-  COMMAND="com.jmstoolkit.logging.console.JTKLogViewerApp"
 fi
+echo "CLASSPATH: $CLASSPATH"
+export CLASSPATH
+COMMAND="com.jmstoolkit.logging.console.JTKLogViewerApp"
 
 JAVA_OPTS="-Djava.util.logging.config.file=logging.properties"
 # Change the name of the properties file:
